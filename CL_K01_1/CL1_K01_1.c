3a5d7b0dde282c4db5e342bd6850f62751d81748
nanbafumiya <bcfn1220200@gn.iwasaki.ac.jp>
2013-01-09 10:31:05 +0900

    応用プログラミングA の課題1

---
+++ CL_K01_1/CL1_K01_1.c
@@ -0,0 +1,10 @@
+/* 作成者：1T1(28)　難波郁也 */
+/* 課題名：課題01_1　*/
+#include <stdio.h>
+
+void main(void) {
+	int sum;
+	sum = 15 + 37;
+	printf("15に37を足した値は%dです。 \n", sum);
+	printf("Press any key to continue \n");
+}
