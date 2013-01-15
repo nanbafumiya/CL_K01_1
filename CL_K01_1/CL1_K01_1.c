12ffa22bff2f487918947aba30bc8062b5dd0e24
nanbafumiya <bcfn1220200@gn.iwasaki.ac.jp>
2013-01-15 21:19:39 +0900

    答えの表示を変更

--- CL_K01_1/CL1_K01_1.c
+++ CL_K01_1/CL1_K01_1.c
@@ -3,9 +3,10 @@
 #include <stdio.h>
 
 void main(void) {
-	int sum1,sum2;
+	int sum1,sum2,sum3;
 	sum1 = 15;
 	sum2 = 37;
-	printf("%d+%d=%dB \n", sum1,sum2,sum1+sum2);
+	sum3 = sum1 + sum2;
+	printf("%d+%d=%dB \n", sum1,sum2,sum3);
 	printf("Press any key to continue \n");
 }
